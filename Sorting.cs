b86020e2bcbf80be1dbc115ebb0dc7c542d0fc40
Kondreddy Saikiran reddy <67851495+saikiran37130204@users.noreply.github.com>
2021-05-10 18:50:10 +0530

    Add files via upload

---
+++ Sorting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_3
+{
+    class Sorting
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int num =1;
+            Console.WriteLine("please enter the numbers");
+            while (num != 0)
+            {
+                num = Int32.Parse(Console.ReadLine());
+                if (num != 0)
+                {
+                    list.Add(num);
+                }
+            }
+            Console.WriteLine("the numbers after sorting");
+            list.Sort();
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
