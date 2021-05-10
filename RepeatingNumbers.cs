6735f9621fac3c8e3d1ba2b2fe821f04677a9c85
Kondreddy Saikiran reddy <67851495+saikiran37130204@users.noreply.github.com>
2021-05-10 18:49:18 +0530

    Add files via upload

---
+++ RepeatingNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_3
+{
+    class RepeatingNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int num = 0;
+            Console.WriteLine("please enter the numbers");
+            while(num>=0)
+            {
+                num = Int32.Parse(Console.ReadLine());
+
+                list.Add(num);
+            }
+            Console.WriteLine("repeating numbers are");
+            for(int i=0;i<list.Count;i++)
+            {
+                for(int j=i+1;j<list.Count;j++)
+                {
+                    if(list[i]==list[j])
+                    {
+                        Console.WriteLine(list[i]);
+                    }
+                }
+            }
+        }
+    }
+}
